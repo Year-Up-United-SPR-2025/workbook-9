910d8a82c544900f64b89c4554342efd69c1111a
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-06-16 10:39:38 -0400

    added comments

--- demo/src/main/java/com/pluralsight/demo/DemoApplication.java
+++ demo/src/main/java/com/pluralsight/demo/DemoApplication.java
@@ -6,11 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+//Container for app
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context;
 		context = SpringApplication.run(DemoApplication.class, args);
+
+		for (String bean : context.getBeanDefinitionNames()){
+			//using beans uses A LOT of beans and shows you the beans that Spring Boot Uses
+			System.out.println(bean);
+		}
 	}
 
 }
