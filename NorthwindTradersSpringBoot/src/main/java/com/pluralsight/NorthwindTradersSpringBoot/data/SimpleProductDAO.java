e87648510fea9026b2b7df9955bb31646180c6df
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-06-16 14:25:42 -0400

    added sql implementation

--- NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/data/SimpleProductDAO.java
+++ NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/data/SimpleProductDAO.java
@@ -4,23 +4,24 @@ import com.pluralsight.NorthwindTradersSpringBoot.models.Product;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-@Component
+
 public class SimpleProductDAO implements ProductDAO {
     private List<Product> products = new ArrayList<>();
     //private HashMap<Integer, Product> products = new HashMap<Integer, Product>();
 
-    public SimpleProductDAO() {
+    public SimpleProductDAO(){
         //this.products.put(1, new Product(1,"Milk", "Food", 10.99 ));
         //this.products.put(2, new Product(2,"Apple", "Food", 1.99 ));
         //this.products.put(3, new Product(3,"Pant", "Clothing", 15.99 ));
         //this.products.put(4, new Product(4,"Shirt", "Clothing", 19.99 ));
 
-        this.products.add(new Product(1, "Milk", "Food", 10.99));
-        this.products.add(new Product(2, "Apple", "Food", 1.99));
-        this.products.add(new Product(3, "Pant", "Clothing", 15.99));
-        this.products.add(new Product(4, "Shirt", "Clothing", 19.99));
+        this.products.add(new Product(1,"Milk", "Food", 10.99 ));
+        this.products.add(new Product(2,"Apple", "Food", 1.99 ));
+        this.products.add(new Product(3,"Pant", "Clothing", 15.99 ));
+        this.products.add(new Product(4,"Shirt", "Clothing", 19.99 ));
     }
 
     @Override
@@ -33,6 +34,7 @@ public class SimpleProductDAO implements ProductDAO {
         //  return (List<Product>) this.products.values();
         return this.products;
     }
+
 
 
 }
