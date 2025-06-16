dda659d5a444ff68faf3c96fad672cad80174b60
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-06-16 11:41:26 -0400

    added more code examples to the demo

--- demo/src/main/java/com/pluralsight/demo/AnotherCLI.java
+++ demo/src/main/java/com/pluralsight/demo/AnotherCLI.java
@@ -1,12 +1,12 @@
-//package com.pluralsight.demo;
-//
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class AnotherCLI implements CommandLineRunner {
-//    @Override
-//    public void run(String ... args) throws Exception{
-//        System.out.println("Hello From AnotherCLI");
-//    }
-//}
+package com.pluralsight.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnotherCLI implements CommandLineRunner {
+    @Override
+    public void run(String ... args) throws Exception{
+        System.out.println("Hello From AnotherCLI");
+    }
+}
