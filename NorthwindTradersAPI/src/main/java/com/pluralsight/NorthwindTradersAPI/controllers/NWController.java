613cee17669b91c7a911b9e882d628e9f9cbc1bc
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-06-17 10:32:40 -0400

    completed ex 1 pg 111

--- NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/NWController.java
+++ NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/NWController.java
@@ -1,4 +1,4 @@
-package com.pluralsight.NorthwindTradersAPI.Controllers;
+package com.pluralsight.NorthwindTradersAPI.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class NWController {
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String index(@RequestParam(defaultValue = "World") String name) {
-        return "<h1>Hello" + name + " </h1> <p>Welcome to the Northwind Database</p>";
+    public String index(@RequestParam(defaultValue = "World") String country) {
+        return "<h1>Hello" + country + " </h1> <p>Welcome to the Northwind Database</p>";
     }
 }
