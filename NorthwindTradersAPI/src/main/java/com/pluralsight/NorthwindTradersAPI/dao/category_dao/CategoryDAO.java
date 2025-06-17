d8ccaf4537f44a37c9652e56e0964826185cfd79
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-06-17 15:50:10 -0400

    CategoryDAO.java should be done

--- NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/dao/category_dao/CategoryDAO.java
+++ NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/dao/category_dao/CategoryDAO.java
@@ -1,4 +1,11 @@
 package com.pluralsight.NorthwindTradersAPI.dao.category_dao;
 
+import com.pluralsight.NorthwindTradersAPI.models.Category;
+
+import java.util.List;
+
 public interface CategoryDAO {
+    List<Category> getAllCategories();
+    Category getCategoryById(int categoryId);
+    void addCategory(Category category);
 }
