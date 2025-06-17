c6dc3afa12ac579443bb0922d37dbf8bde0174f5
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-06-17 09:55:42 -0400

    WebPage Demo

--- demo/src/main/java/com/pluralsight/demo/DemoApplication.java
+++ demo/src/main/java/com/pluralsight/demo/DemoApplication.java
@@ -1,10 +1,5 @@
 package com.pluralsight.demo;
 
-
-import com.pluralsight.demo.dao.RegistrationDAO;
-import com.pluralsight.demo.dao.SimpleRegistrationDAO;
-import com.pluralsight.demo.model.Student;
-import com.pluralsight.demo.service.RegistrationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
